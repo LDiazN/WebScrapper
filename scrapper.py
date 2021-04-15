2c6c5418fa524c73144d4462141d4022ceecf7b9
Luis Diaz <ldiazn98@gmail.com>
2021-04-15 14:31:55 -0400

    initial try for ElPitazo Scrapper

---
+++ scrapper.py
@@ -0,0 +1,53 @@
+"""
+    Main functions you can call in other places
+"""
+from scrapy.crawler import CrawlerProcess
+from scrapy         import signals
+from settings import URL_TO_SPIDERS, CRAWLER_SETTINGS
+
+_process = CrawlerProcess(CRAWLER_SETTINGS)
+
+def scrape(url : str, additional_data = {}):
+    """
+        Main scrapper function 
+    """
+    # Items accumulator
+    items = []
+    
+    # callback function to collect items on the fly
+    def items_scrapped(item, response, spider): 
+        items.append(item)
+
+    # Get corresponding spider from url
+    spider = _get_spider_from_url(url)
+    spider.start_urls = [url]
+
+    # create crawler for this spider, connect signal so we can collect items
+    crawler = _process.create_crawler(spider)
+    crawler.signals.connect(items_scrapped, signal=signals.item_scraped)
+
+    # start scrapping
+    _process.crawl(crawler)
+    _process.start()
+
+    # return post processed scrapped objects
+    return _process_items_into_final_data(items)
+
+def _get_spider_from_url(url : str):
+    """
+        Validates if this url is scrapable and returns its 
+        corresponding spider when it is
+    """
+
+    for known_url in URL_TO_SPIDERS.keys():
+        if known_url in url:
+            return URL_TO_SPIDERS[known_url]
+
+    raise ValueError(f"Unable to scrap this site: {url}")
+
+
+def _process_items_into_final_data(items):
+    """
+        Post process a list of items returned by the spiders
+    """
+    return items
